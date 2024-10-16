c70f0dcb4d07994e8b901c401d26a668a9ae817f
AndresRamirez917 <andresmintic2021@gmail.com>
2024-10-16 13:02:35 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,35 @@
+async function getData(){
+    const result = await fetch('https://rickandmortyapi.com/api/character');
+    const character = await result.json();
+    const characterArr = character.results.map(elemento => Object.entries(elemento));
+    const characterArrSliced = characterArr.slice(0,4);
+    console.log(characterArr);
+    character.results.forEach(element => {
+        const ranInt = randomData(1, characterArr.length);
+        const ranIndex = ranInt;
+        for(i = 0; i < characterArrSliced.length; i++){
+            console.log("dmadre")
+            if(element.id == i){
+                const box = document.createRange().createContextualFragment(`
+                    
+              
+            <div class="box">
+                <img src="${characterArr[ranIndex][8][1]}" alt="">
+                <h2>Bussiness</h2>
+                <p>Lorem ipsum dolor sit amet consectetur. In pretium pretium feugiat mus sed. Semper laoreet odio tincidunt praesent amet egestas porttitor scelerisque ut.</p>
+                <a href="">get started</a>
+            </div>
+            
+                    
+                    
+                    `)
+                    const courses_row = document.getElementById('courses');
+                    courses_row.append(box);
+            }
+        }
+        function randomData(min, max) {
+            return Math.floor(Math.random() * (min - max + 1) + max)
+        }
+    });
+}
+getData()
